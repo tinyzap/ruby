f3158b43082a7a4a22cd4dc229b2045918279947
Brad Gessler <bradgessler@gmail.com>
2022-06-24 14:32:50 -0700

    Fix eager loading of ENV vars

--- lib/imageomatic/configuration.rb
+++ lib/imageomatic/configuration.rb
@@ -3,11 +3,11 @@ module Imageomatic
     attr_accessor :secret_key, :public_key
 
     def load_secret_key_env
-      self.secret_key = ENV.fetch("IMAGEOMATIC_SECRET_KEY")
+      self.secret_key = ENV["IMAGEOMATIC_SECRET_KEY"]
     end
 
     def load_public_key_env
-      self.public_key = ENV.fetch("IMAGEOMATIC_PUBLIC_KEY")
+      self.public_key = ENV["IMAGEOMATIC_PUBLIC_KEY"]
     end
 
     def load_env
